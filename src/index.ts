bebb8f7ae21f0753b6e620e9ab877401d1b926c5
emertola <emertola@gmail.com>
2024-04-22 12:00:52 +0800

    chore: initial setup

---
+++ src/index.ts
@@ -0,0 +1,22 @@
+import express from 'express';
+import dotenv from 'dotenv';
+import cookieParser from 'cookie-parser';
+import session from 'express-session';
+import passport from 'passport';
+import routes from './routes';
+
+dotenv.config();
+
+const app = express();
+const PORT = process.env.HOST_PORT || 3001;
+
+app.use(express.json());
+app.use(cookieParser());
+app.use(
+  session({ secret: 'session123', resave: false, saveUninitialized: false })
+);
+app.use(passport.initialize());
+app.use(passport.session());
+app.use('/api/v1', routes);
+
+app.listen(PORT, () => console.log(`Running express server on PORT ${PORT}`));
